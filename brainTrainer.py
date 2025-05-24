5f54841d8e3f12f88a4ca1106d10b04eac3412e3
pdziergas <114980375+pdziergas@users.noreply.github.com>
2025-05-24 19:14:52 +0200

    Created training on multiple results without expected results filtering

--- brainTrainer.py
+++ brainTrainer.py
@@ -157,3 +157,57 @@ class SingleImageMultipleResultsBrainTrainer():
     
     def get_model(self):
         return self.brain
+    
+class SingleImageMultipleResultsBrainTrainer():
+    def __init__(self, 
+                image_tensor: torch.tensor, 
+                expected_result_list: torch.tensor, 
+                device: str, 
+                loss_function: nn.MSELoss):
+       
+       self.image_tensor = image_tensor
+       self.expected_result_list = expected_result_list 
+       self.brain = CNN_Brain.CNN().to(device)
+       self.optimizer = torch.optim.SGD(self.brain.parameters(), lr=0.001, momentum=0.9)
+       self.loss_function = loss_function
+    
+    def epoch_train(self, index: int):
+
+        for result in self.expected_result_list:
+            result = torch.unsqueeze(result, 0)
+            
+            for i in range(100):
+                self.optimizer.zero_grad()
+
+
+                cnn_result = self.brain(self.image_tensor)
+                loss = self.loss_function(cnn_result, result)
+                loss.backward()
+                self.optimizer.step()
+            return loss
+        
+    def train_brain(self, epochs_num: int):
+        best_vloss = 1_000_000.
+            
+        for i in range(epochs_num):
+                
+            print("Epoch: " + str((i+1)))
+            self.brain.train(True)
+            loss_for_epoch = self.epoch_train(i)
+                
+            self.brain.eval()
+            torch.no_grad()
+            avg_loss = loss_for_epoch / (i+1)
+            print("Loss: {}".format(avg_loss))
+                
+            if avg_loss < best_vloss:
+                best_vloss = avg_loss
+                brain_filename = 'models/single_image_multiple_results_brain_{}'.format((i+1))
+                if  os.path.exists(brain_filename):
+                    os.remove(brain_filename)
+                    
+                torch.save(self.brain.state_dict(), brain_filename)
+        return
+    
+    def get_model(self):
+        return self.brain
--- main.py
+++ main.py
@@ -12,10 +12,6 @@ from torchvision import transforms
 from PIL import Image
 import brainTrainer as trainer
 import DataFilter
-from ImageLoader import ImageLoader as im
-from ColorsForImage import ColorsForImage as cfi
-from Z_Score_Handler import Z_Score_Handler as z_score_handler
-from ImageColorDataSet import ImageColorDataSet as icd
 
 def rgb_to_tensor(rgb: str):
 
